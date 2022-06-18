bd50472995857ec3988c357467d07bde4481df2a
hann1010 <hanna.paivasto@gmail.com>
2022-06-18 22:10:26 +0300

    set chat_view

--- chat/views.py
+++ chat/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'chat/index.html')
+
+
+def chat_view(request):
+    return render(request, 'chat/chat_view.html')
