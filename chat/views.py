c113985c1bbf59eee8d5c2fd4e1efa3b49ae04b3
hann1010 <hanna.paivasto@gmail.com>
2022-06-22 19:50:00 +0300

    set chat_view

--- chat/views.py
+++ chat/views.py
@@ -40,5 +40,5 @@ class Chat_View(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         form.instance.post_type = 'Topic'
-        messages.add_message(self.request, messages.INFO, 'Yours new topic has been saved!')
+        messages.add_message(self.request, messages.INFO, 'Yours new message has been saved!')
         return super().form_valid(form)
