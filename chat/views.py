98c2cae2c5f14993741b2e60d5535de12215869e
hann1010 <hanna.paivasto@gmail.com>
2022-06-26 21:37:17 +0300

    set chat_view

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Chat_post
 from django.contrib import messages
+from django.core.paginator import Paginator
 #from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 #from django.contrib.auth.decorators import login_required
@@ -23,7 +24,12 @@ class Chat_View(LoginRequiredMixin, CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["chat_context"] = Chat_post.objects.all().order_by('-date_posted')
+        items_in_page_int = self.request.user.profile.items_in_page
+        db_data = Chat_post.objects.all().order_by('-date_posted')
+        paginator = Paginator(db_data, items_in_page_int)
+        page_number = self.request.GET.get('page')
+        page_data = paginator.get_page(page_number)
+        context["chat_context"] = page_data
         context["title"] = 'Chat'
         return context
 
