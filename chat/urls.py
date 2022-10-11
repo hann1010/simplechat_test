b9bae943935fdd061ecc1b6d880e3b4dce7f75e7
hann1010 <hanna.paivasto@gmail.com>
2022-10-11 21:49:34 +0300

    clean up

--- chat/urls.py
+++ chat/urls.py
@@ -17,15 +17,7 @@ urlpatterns = [
     path('', views.home, name='chat-home'),
     path('chat/chat-view/chat_json/', views.jsonChat, name='chat-json'),
     path('chat/chat-view/', Chat_View.as_view(), name='chat-view'),
-    #path('latest/comments/', views.latest_comments, name='forum-latest_comments'),
-    #path('latest/all/', views.latest_all, name='forum-latest_all'),
-    #path('topic/new/', TopicCreateView.as_view(), name='forum_topic-create'),
-    #path('all/<int:pk>/comment/new/', CommentCreateView.as_view(), name='forum_comment-create'),
-    #path('all/<int:pk>/open/', AllDetailView.as_view(), name='forum_open_one_post'),
-    #path('all/<int:pk>/thread/', ThreadDetailView.as_view(), name='forum_thread'),
     path('chat/<int:pk>/update/', ChatUpdateView.as_view(), name='chat_update'),
     path('chat/<int:pk>/delete/', ChatDeleteView.as_view(), name='chat_delete'),
     path('chat/<int:pk>/user_info/', UserDetailView.as_view(), name='chat-user_info'),
-    
-	
 ]
