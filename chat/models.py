1d678783073c49195ae356192756bec9ebc13ba1
hann1010 <hanna.paivasto@gmail.com>
2022-11-05 21:19:57 +0200

    fix models

--- chat/models.py
+++ chat/models.py
@@ -12,6 +12,7 @@ class Chat_post(models.Model):
     date_posted = models.DateTimeField(default=timezone.now)
     date_last_save = models.DateTimeField(auto_now=timezone.now)
     author_name = models.CharField(max_length=100, blank=True)
+    author_nickname = models.CharField(max_length=100, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
