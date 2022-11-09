2f27fc43e386a5ad4240b9f2655e162fc74ef31e
hann1010 <hanna.paivasto@gmail.com>
2022-11-09 23:57:19 +0200

    jsonResponse

--- chat/forms.py
+++ chat/forms.py
@@ -5,7 +5,7 @@ from ckeditor.widgets import CKEditorWidget
 
 class Chat_view_Form(forms.ModelForm):
     content = forms.CharField(max_length=500, widget=CKEditorWidget(attrs={'cols':110, 'rows': 5,
-     'placeholder': 'Say something..','class': 'form-control'}))
+         'placeholder': 'Say something..','class': 'form-control'}))
     class Meta:
         model = Chat_post
         fields = ['content']
