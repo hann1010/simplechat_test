c8fa8bd2c26313bd2e6c557ce0d64944e2c0640e
hann1010 <hanna.paivasto@gmail.com>
2022-11-17 23:46:11 +0200

    add ChatCreateView

--- chat/urls.py
+++ chat/urls.py
@@ -6,6 +6,7 @@ from .views import (
     #ThreadDetailView,
     #TopicCreateView,
     #CommentCreateView,
+    ChatCreateView,
     ChatUpdateView,
     ChatDeleteView,
     UserDetailView
@@ -17,6 +18,7 @@ urlpatterns = [
     path('', views.home, name='chat-home'),
     path('chat/chat-view/chat_json/', views.jsonChat, name='chat-json'),
     path('chat/chat-view/', Chat_View.as_view(), name='chat-view'),
+    path('chat/chat-new/', ChatCreateView.as_view(), name='chat-new'),
     path('chat/<int:pk>/update/', ChatUpdateView.as_view(), name='chat_update'),
     path('chat/<int:pk>/delete/', ChatDeleteView.as_view(), name='chat_delete'),
     path('chat/<int:pk>/user_info/', UserDetailView.as_view(), name='chat-user_info'),
