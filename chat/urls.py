a6e451534dacdfde0fa92c5779d417a9a114a5e3
hann1010 <hanna.paivasto@gmail.com>
2022-11-24 21:16:34 +0200

    add ChatCommentCreateView

--- chat/urls.py
+++ chat/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('chat/chat-view/', Chat_View.as_view(), name='chat-view'),
     path('chat/chat-new/', ChatCreateView.as_view(), name='chat-new'),
     path('chat/<int:pk>/update/', ChatUpdateView.as_view(), name='chat_update'),
-    path('chat/<int:pk>/comment/', ChatCommentCreateView.as_view(), name='chat_update'),
+    path('chat/<int:pk>/comment/', ChatCommentCreateView.as_view(), name='chat_comment'),
     path('chat/<int:pk>/delete/', ChatDeleteView.as_view(), name='chat_delete'),
     path('chat/<int:pk>/user_info/', UserDetailView.as_view(), name='chat-user_info'),
     ]
